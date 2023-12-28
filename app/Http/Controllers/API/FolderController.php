5e98c17dec49fb8614f6fe44dea5b1603c55a323
brakthin123 <brakthin904@gmail.com>
2023-12-28 12:38:01 +0700

    update ke folder to lower case

--- app/Http/Controllers/API/FolderController.php
+++ app/Http/Controllers/API/FolderController.php
@@ -24,7 +24,10 @@ class FolderController extends Controller
         $user = JWTAuth::parseToken()->authenticate();
         $folder = $user->folders()->create($data);
 
-        return response()->json(['message' => 'Folder created successfully', 'folder' => $folder]);
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Folder created successfully',
+            'data' => $folder]);
     }
 
     public function index(Request $request)
